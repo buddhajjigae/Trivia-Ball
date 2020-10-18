592e5f735d1585274b78be8c52afb2971c90cdfd
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-10-18 13:43:47 -0400

    Add files via upload
    Adding scraper and csv_script

---
+++ csv_script.py
@@ -0,0 +1,251 @@
+import csv
+import os
+import pathlib
+import pymysql
+import pandas as pd
+from os import path
+
+
+#########################################################
+'''                      METHODS                      ''' 
+#########################################################
+
+def get_player_id(file):
+    counter = 0
+    
+        if i == 
+        
+
+def write_csv(filename, data):
+    with open(filename, 'a') as csvfile:  
+            writer = csv.writer(csvfile, delimiter = ',')
+            i = 0
+            for e in data:
+                writer.writerow(data[0+i]) 
+                i += 1
+
+
+def write_completed(url):
+    with open(r'C:\NBA Scraper\Fixed_Completed.txt', 'a') as txtfile:
+        txtfile.write(url + "\n")
+
+
+def write_files_toFix(files):
+    with open(r"C:\NBA Scraper\File_Log.txt", "a") as textfile:
+        for file in files:
+            textfile.write(file + "\n")
+                
+#########################################################
+'''                    MAIN CODE                      ''' 
+#########################################################
+
+'''Creating Necessary Files'''
+if path.exists(r"C:\NBA Scraper\Fixed_Completed.txt") == False:
+    os.chdir(r"C:\NBA Scraper\\")
+    comp_file = open('Fixed_Completed.txt','w')
+    comp_file.close()
+    print("Created Fixed_Completed.txt")
+else:
+    print(r"C:\NBA Scraper\Fixed_Completed.txt   File Exists, Will Not Create File")
+    
+    
+if path.exists(r"C:\NBA Scraper\File_Log.txt") == False:
+    os.chdir(r"C:\NBA Scraper\\")
+    comp_file = open('File_Log.txt','w')
+    comp_file.close()
+    print("Created File_Log.txt")
+else:
+    print(r"C:\NBA Scraper\File_Log.txt   File Exists, Will Not Create File")
+
+
+'''SQL Database Connection'''
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='buddhajjigae',
+                             db='nba_scraper',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+cursor_sql = connection.cursor()
+
+'''Column Names for SQL Table'''
+c1 = "ranker INT, game_season INT, date_game TEXT, age TEXT, team_id TEXT,"
+c2 = "game_location TEXT, opp_id TEXT, game_result TEXT, gs TEXT, mp TEXT,"
+c3 = "fg INT, fga INT, fg_pct DOUBLE, fg3 INT, fg3a INT, fg3_pct DOUBLE,"
+c4 = "ft INT, fta INT, ft_pct DOUBLE, orb INT, drb INT, trb INT, ast INT,"
+c5 = "stl INT, blk INT, tov INT, pf INT, pts INT, game_score DOUBLE,"
+c6 = "plus_minus TEXT, player_id VARCHAR(20), playoff TEXT);"
+c_names = "CREATE TABLE `nba_scraper`.`players` (" + c1 + c2 + c3 + c4 + c5 + c6
+
+
+'''Make player_id a primary key'''
+p_key = "ALTER TABLE players ADD PRIMARY KEY (player_id);"
+
+'''Insert row into db'''
+sql_1 = "INSERT INTO players (ranker, game_season, date_game, age, team_id, "  
+sql_2 = "game_location, opp_id, game_result, gs, mp, fg, fga, fg_pct, fg3, "
+sql_3 = "fg3a. fg3_pct, ft, fta, ft_pct, orb, drb, trb, ast, stl, blk, tov "
+sql_4 = "pf, pts, game_score, plus_minus, player_id, playoff) VALUES "
+sql_5 = "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, "
+sql_6 = "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"       
+sql = sql_1 + sql_2 + sql_3 + sql_4 + sql_5 + sql_6
+
+'''Check if players table exists in DB'''
+with connection.cursor() as cursor:
+        # Create a new record
+        sql = "SHOW TABLES LIKE 'players'"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        if result:
+            print('Players table exists in SQL DB, will not create table.')
+        else:
+            cursor.execute(c_names)
+            cursor.execute(p_key)
+            print('Players table created in SQL DB')    
+
+
+#directory = r'C:\NBA Scraper\players'
+
+'''Files in directory'''
+file_list = []
+
+'''Already completed files'''
+comp_files_raw = []
+comp_files = []
+
+'''name_types are the 'Did Not Play, Inactive, etc.'''
+name_types_list = []
+name_types_set = []
+
+
+# '''Files that have been completed'''
+# clean_comp = []
+
+'''
+for file in os.listdir(directory):
+    file_list.append(file + ".csv")
+'''
+
+
+for path_a, subdirs, files in os.walk(r'C:\NBA Scraper\players'):
+    for name in files:
+        file_path = str(pathlib.PurePath(path_a, name))        
+        file_list.append(file_path)
+        
+print(len(file_list))
+
+
+with open(r'C:\NBA Scraper\Fixed_Completed.txt', 'r') as comp_file:
+    comp_files_raw = comp_file.readlines()
+
+for file in comp_files_raw:
+    file = file.replace('\n','')
+    comp_files.append(file)
+
+print(comp_files)    
+
+
+'''Counters'''
+clean_count = 0
+no_count = 0
+
+    
+for file in file_list:    
+    raw_list = []
+    fixed_list = []
+    
+    for e in file:
+        
+    
+    
+    if file not in comp_files and file[-8:] != 'Test.csv':
+        clean_count += 1
+        playoffs = file[-14:-4]
+        with open(file, 'r') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+        
+                for row in csv_reader:
+                    if len(row) != 0:
+                        raw_list.append(row)
+                                    
+                for i in raw_list:
+                    for j,e in enumerate(i):
+                        if i[j] == '' and j != 5:
+                            i[j]= 'NULL'                              
+                        elif e == 'Did Not Play':
+                            i.extend(['NULL'] * 21)
+                        elif e == 'Did Not Dress':
+                            i.extend(['NULL'] * 21)
+                        elif e == 'Player Suspended':
+                            i.extend(['NULL'] * 21)
+                        elif e == 'Inactive':
+                            i.extend(['NULL'] * 21)
+                        elif e == 'Not With Team':
+                            i.extend(['NULL'] * 21)
+                                                   
+                        name_types_list.append(i[8])
+                    
+                    
+                    if playoffs == 'playoffs':
+                        i.extend('Yes')
+                    else:
+                        i.extend('No')
+                    cursor_sql.execute(sql, i)
+                    #pd_list = pd_list.append(i)
+                    #df = pd.DataFrame(i)
+                    #print(df)
+                    fixed_list.append(i)
+                    #print('FILE DONE: ' + file) 
+    else:
+        no_count += 1    
+        
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+           
+        
+        
+# for file in file_list:    
+#     raw_list = []
+#     fixed_list = []
+    
+#     if file not in comp_files and file[-8:] != 'Test.csv':
+#         clean_count += 1
+#         with open(file, 'r') as csv_file:
+#                 csv_reader = csv.reader(csv_file, delimiter=',')
+        
+#                 for row in csv_reader:
+#                     if len(row) != 0:
+#                         raw_list.append(row)
+                                    
+#                 for i in raw_list:
+#                     for j,e in enumerate(i):
+#                         if i[j] == '' and j != 5:
+#                             i[j]= 'NULL'                              
+#                         elif e == 'Did Not Play':
+#                             i.extend(['NULL'] * 21)
+#                         elif e == 'Did Not Dress':
+#                             i.extend(['NULL'] * 21)
+#                         elif e == 'Player Suspended':
+#                             i.extend(['NULL'] * 21)
+#                         elif e == 'Inactive':
+#                             i.extend(['NULL'] * 21)
+#                         elif e == 'Not With Team':
+#                             i.extend(['NULL'] * 21)
+                                                   
+#                         name_types_list.append(i[8])    
+#                     print(i)
+#                     fixed_list.append(i)
+#                 #print('FILE DONE: ' + file)
+#                 test = file.replace('.csv','_Test.csv')   
+#                 write_csv(test, fixed_list) 
+#         write_completed(file)  
+#     else:
+#         no_count += 1    
+         
+ 
+           
+# name_types_set = set(name_types_list)
+# name_types_list = list(name_types_set)
+# print(name_types_list)      
+# print("***CLEANER HAS FINISHED***")
+# print("Cleaned: " + str(clean_count) + " Files")
+# print("Ignored: " + str(no_count) + " Files")
---
+++ scraper.py
@@ -0,0 +1,319 @@
+import requests
+import csv
+import sys
+import re
+import os
+from bs4 import BeautifulSoup
+from string import ascii_lowercase
+from time import sleep
+from os import path
+
+
+#########################################################
+'''                      METHODS                      ''' 
+#########################################################
+
+'''Generates the name of the file to be created'''
+def file_name(URL, playername):
+    l = URL.split('/')[-3:]
+    filename = playername + '-' + '-'.join(l) + ".csv"
+    
+    if filename[-5:] == "-.csv":
+        filename = filename.replace("-.csv", ".csv")
+        
+    return filename
+
+
+'''Finds the URL of each Game Log for the Player'''
+def get_game_logs(url):
+    game_url = 'https://www.basketball-reference.com' + url
+    result = []
+    
+    page = requests.get(game_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    logs = soup.find("li", { "class" : "full hasmore"})
+
+    for i in logs:
+        td = logs.find_all('a')
+        for j in td:
+            glog = j.get('href')
+            result.append('https://www.basketball-reference.com' + glog)
+    result = list(dict.fromkeys(result))
+    
+    return result
+
+
+'''Gets the headers of the data table'''
+def get_headers(table):
+    thead = table.find('thead')
+    th_head = thead.find_all('th')
+    fields = []
+
+    for thh in th_head:
+        fields.append(thh.get('data-stat'))
+    
+    return fields
+
+
+'''Gets the players that are currently playing'''
+def get_players(url):
+    player_url = url
+    page = requests.get(player_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    table = soup.find("table", { "id" : "players" })
+    names = player_names(table)
+    
+    return names
+
+
+'''Gets the player name for the file'''
+def get_player_name(name):
+    for n in name:
+        n1 = name.string
+        n2 = re.split(' \d+', n1)
+        n2 = n2[0]
+
+    j = n2.split()
+    
+    if len(j) == 2:
+        player_name = j[1] + "_" + j[0]
+    else:
+        player_name = j[1] + "_" + j[0] + "_" + j[2]
+   
+    if os.path.isdir(player_name):
+        return player_name
+    else:     
+        os.mkdir(player_name)
+
+    return player_name
+
+
+'''Generates a list of all the data values'''
+def make_table_list(table):
+    result = []
+    table_rows = table.find_all('tr')
+    
+    for tr in table_rows:
+        td = tr.find_all('th') + tr.find_all('td')
+        row = [i.text for i in td]
+        result.append(row)
+    
+    for i, e in enumerate(result):
+        if e[0] == "Rk":
+            result.pop(i)
+    
+    return result
+
+
+'''Gets the URL's for each active player'''
+def player_names(table):
+    result = []
+    table_rows = table.find_all('strong')
+    
+    for tr in table_rows:
+        td = tr.find_all('a')
+        #row = [i.get('href') for i in td]
+        for i in td:
+            row = i.get('href')
+            result.append(row)
+
+    return result    
+
+
+'''Writes to completed file to ignore'''
+def write_completed(url):
+    with open(r'C:\NBA Scraper\Completed.txt', 'a') as txtfile:
+        txtfile.write(url + "\n")
+
+
+'''Writes the headers into the CSV file'''
+def write_headers(headers, filename):
+    with open(filename, 'w') as csvfile:  
+        writer = csv.writer(csvfile, delimiter = ',')
+        writer.writerow(headers)  
+
+    
+'''Writes the data rows into the CSV file'''    
+def write_rows(filename, data):
+    with open(filename, 'a') as csvfile:  
+        writer = csv.writer(csvfile, delimiter = ',')
+        i = 0
+        for e in data:
+            writer.writerow(data[0+i]) 
+            i += 1
+    
+
+# '''Writes the URL's that will be scraped into a txt file'''
+# def write_urls_text(names):
+#     textfile = open("Game_Logs.txt", "a")
+    
+#     for i in names:
+#         logs = get_game_logs(i)
+#         for j in logs: 
+#             textfile.write(j + "\n")
+            
+#     print("Finished File:   Game_Logs.txt")      
+
+
+'''Writes the URL's that will be scraped into a txt file'''
+def write_urls_text(names, alphabet):
+    with open(r"C:\NBA Scraper\Game_Logs.txt", "a") as textfile:
+        for i in names:
+            logs = get_game_logs(i)
+            for j in logs: 
+                textfile.write(j + "\n")
+            
+    print("Finished Writing To File:   Game_Logs.txt || Letter: " + alphabet)      
+
+
+#########################################################
+'''                    MAIN CODE                      ''' 
+#########################################################
+
+print("***STARTING NBA SCRAPER***")
+
+'''Generate Necessary Directories and Files'''
+if os.path.isdir(r"C:\NBA Scraper") == True:
+    print(r"C:\NBA Scraper\   Directory Exists, Will Not Create Directory")
+else:
+    os.chdir(r"C:\\")
+    os.mkdir("NBA Scraper")
+    print(r"C:\NBA Scraper\   Created")
+
+
+if os.path.isdir(r"C:\NBA Scraper\players") == True:
+    print(r"C:\NBA Scraper\players   Directory Exists, Will Not Create Directory")
+else:
+    os.chdir(r"C:\NBA Scraper\\")
+    os.mkdir("players")
+    print(r"C:\NBA Scraper\players   Created")
+    
+    
+if os.path.isdir(r"C:\NBA Scraper\teams") == True:
+    print(r"C:\NBA Scraper\teans   Directory Exists, Will Not Create Directory")
+else:
+    os.chdir(r"C:\NBA Scraper\\")
+    os.mkdir("teams")  
+    print(r"C:\NBA Scraper\teams   Created")
+
+
+if path.exists(r"C:\NBA Scraper\Completed.txt") == False:
+    os.chdir(r"C:\NBA Scraper\\")
+    comp_file = open('Completed.txt','w')
+    comp_file.close()
+    print("Created Completed.txt")
+else:
+    print(r"C:\NBA Scraper\Completed.txt   File Exists, Will Not Create File")
+    
+
+if path.exists(r"C:\NBA Scraper\Game_Logs.txt") == False:
+    os.chdir(r"C:\NBA Scraper\\")
+    logs = open('Game_Logs.txt','w')
+    logs.close()
+    print("Created Game_Logs.txt")
+else:
+    os.remove(r'C:\NBA Scraper\Game_Logs.txt')
+    os.chdir(r"C:\NBA Scraper\\")
+    logs = open('Game_Logs.txt','w')
+    logs.close()
+    print("Created Game_Logs.txt")
+
+    
+'''ITERATE THROUGH SITE FOR TESTING'''
+names = get_players('https://www.basketball-reference.com/players/' + 'a')
+write_urls_text(names, 'a')
+
+
+# '''Generate Game Logs'''
+# for i in ascii_lowercase:
+#     names = get_players('https://www.basketball-reference.com/players/' + i)
+#     write_urls_text(names, i)
+
+ 
+'''Read Game_Logs.txt and delete after reading'''
+#logs_file = open(r'C:\NBA Scraper\Game_Logs.txt','r')
+#logs = logs_file .readlines()
+logs = open(r'C:\NBA Scraper\Game_Logs.txt','r').read().split('\n')
+#logs_file .close()
+print(len(logs))
+os.remove(r'C:\NBA Scraper\Game_Logs.txt')
+print("***Game_Logs.txt DELETED***")
+
+
+'''Read Completed.txt'''
+#comp_file = open(r'C:\NBA Scraper\Completed.txt','r')
+#comp_logs = comp_file.readlines()
+#comp_file.close()
+comp_logs = open(r'C:\NBA Scraper\Completed.txt','r').read().split('\n')
+
+
+'''Scraper Counters'''
+comp_counter = 0
+ncomp_counter = 0
+
+
+'''Player Scraper'''
+for log in logs:
+    if log not in comp_logs:
+        comp_counter += 1
+        URL = log.replace("\n", "")
+        sleep(3)
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        os.chdir(r"C:\NBA Scraper\players")
+        player_name = get_player_name(soup.find('h1', itemprop="name"))
+        filename = file_name(URL, player_name)
+        table = soup.find(class_="table_outer_container")
+        headers = get_headers(table)
+        player_dir = r"C:\NBA Scraper\players\\" + player_name
+        os.chdir(player_dir)
+        write_headers(headers, filename)
+        if URL[-9:] == 'playoffs/':
+            stat_table = soup.find("table", { "id" : "pgl_basic_playoffs" })
+            data = make_table_list(stat_table)
+            write_rows(filename, data)   
+            print("Finished File:   " + filename)
+        else: 
+            stat_table = soup.find("table", { "id" : "pgl_basic" })
+            data = make_table_list(stat_table)
+            write_rows(filename, data)
+            print("Finished File:   " + filename)
+        write_completed(log)
+    else:            
+        ncomp_counter += 1
+
+
+
+'''Show Completion of Scraper'''
+print("***SCRAPER HAS FINISHED***")
+print("Completed: " + str(comp_counter) + " Files")
+print("Ignored: " + str(ncomp_counter) + " URLS")
+
+#-------------------------------
+#-------------------------------
+#-------------------------------
+#-------------------------------
+#-------------------------------
+# for i in logs:
+#     URL = i.replace("\n", "")
+#     sleep(5)
+#     page = requests.get(URL)
+#     soup = BeautifulSoup(page.content, 'html.parser')
+#     os.chdir(r"C:\NBA Scraper\players")
+#     player_name = get_player_name(soup.find('h1', itemprop="name"))
+#     filename = file_name(URL, player_name)
+#     table = soup.find(class_="table_outer_container")
+#     headers = get_headers(table)
+#     player_dir = r"C:\NBA Scraper\players\\" + player_name
+#     os.chdir(player_dir)
+#     write_headers(headers, filename)
+#     if URL[-9:] == 'playoffs/':
+#         stat_table = soup.find("table", { "id" : "pgl_basic_playoffs" })
+#         data = make_table_list(stat_table)
+#         write_rows(filename, data)   
+#         print("Finished File:   " + filename)
+#     else: 
+#         stat_table = soup.find("table", { "id" : "pgl_basic" })
+#         data = make_table_list(stat_table)
+#         write_rows(filename, data)
+#         print("Finished File:   " + filename)
